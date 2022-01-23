b6f5d382d2b3bcbeb3ed220d53b455476d7e2b4b
Mateusz Myalski <mateusz.myalski+github@gmail.com>
2022-01-23 13:23:45 +0100

    Reorganized repo structure. Forked original STM32 linker and startup files.

---
+++ apps/stm32u5/u585xx_base/syscalls.c
@@ -0,0 +1,80 @@
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+extern int errno;
+
+extern int __io_putchar(int ch) __attribute__((weak));
+extern int __io_getchar(void) __attribute__((weak));
+
+char *__env[1] = {0};
+char **environ = __env;
+
+int _kill(int pid, int sig) {
+    errno = EINVAL;
+    return -1;
+}
+
+void _exit(int status)
+{
+    _kill(status, -1);
+    while(1)
+    {
+        __asm("NOP");
+        // Hang the device
+    }
+}
+
+caddr_t _sbrk(int incr)
+{
+    extern char *_end;
+    extern char *_eheap;
+    static char *heap_end;
+    char *prev_heap_end;
+
+    if (heap_end == 0) {
+        heap_end = &_end;
+    }
+
+    prev_heap_end = heap_end;
+    if (heap_end + incr > &_eheap) {
+        errno = ENOMEM;
+        __asm("BKPT");
+        return (caddr_t)-1;
+    }
+
+    heap_end += incr;
+
+    return (caddr_t)prev_heap_end;
+}
+
+int _write(int file, char *ptr, int len)
+{
+    for (int data_idx = 0; data_idx < len; data_idx++)
+    {
+        __io_putchar(*ptr++);
+    }
+
+    return len;
+}
+
+int _read(int file, char *ptr, int len) {
+  for (int data_idx = 0; data_idx < len; data_idx++)
+    {
+        *ptr++ = __io_getchar();
+    }
+
+  return len;
+}
+
+int _close(int file) { return -1; }
+
+int _fstat(int file, struct stat *st)
+{
+    st->st_mode = S_IFCHR;
+    return 0;
+}
+
+int _lseek(int file, int ptr, int dir) { return 0; }
+
+int _isatty(int file) { return 1; }
