1454132df2d7641d45d217a9a7a25c899d2181e0
Mateusz Myalski <mateusz.myalski+github@gmail.com>
2022-01-28 15:19:42 +0100

    Main cleanup.

--- apps/stm32u5/u585xx_bring-up/main.c
+++ apps/stm32u5/u585xx_bring-up/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "stm32u585xx.h"
+
 #include "gpio.h"
 
 int main() {
@@ -9,17 +9,15 @@ int main() {
     hal_gpio_ioctl(GPIOH, 6, gpio_ioctl_output);
     hal_gpio_ioctl(GPIOH, 7, gpio_ioctl_output);
 
-    
-
     while (1) {
         hal_gpio_write(GPIOH, 6, 1);
         hal_gpio_write(GPIOH, 7, 0);
-        for(int i = 0; i < 100000; i++){
+        for (int i = 0; i < 100000; i++) {
             __asm("NOP");
         }
         hal_gpio_write(GPIOH, 6, 0);
         hal_gpio_write(GPIOH, 7, 1);
-        for(int i = 0; i < 100000; i++){
+        for (int i = 0; i < 100000; i++) {
             __asm("NOP");
         }
         __asm("LDR  r0, = 0xAAAAAAAA");
