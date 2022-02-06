3876068cb66c91ec2944aee3cad4f9bf0aea98cb
MateuszMyalski <59411812+MateuszMyalski@users.noreply.github.com>
2022-02-06 20:06:57 +0100

    I2C master mode - basic driver
    * I2C driver implementation
    * I2C1 and I2C2 bsp init
    * HTS221 communication test

--- apps/stm32u5/u585xx_bring-up/bsp/periphs.c
+++ apps/stm32u5/u585xx_bring-up/bsp/periphs.c
@@ -2,50 +2,45 @@
 #include "gpio.h"
 #include "i2c.h"
 
-static int init_i2c2() {
-    int err = 0;;
+static int init_i2c(I2C_TypeDef* I2C, GPIO_TypeDef* I2C_SDA_port, uint32_t I2C_SDA_pin, GPIO_TypeDef* I2C_SCL_port,
+                uint32_t I2C_SCL_pin) {
+    int err = 0;
 
     /* SDA GPIO */
-    // No internal pull-ups
-    err = hal_gpio_open(BSP_I2C2_SDA_PORT);
+    err = hal_gpio_open(I2C_SDA_port);
     ExitOnError(err);
 
-    err = hal_gpio_ioctl(BSP_I2C2_SDA_PORT, BSP_I2C2_SDA_PIN, gpio_ioctl_output);
+    err = hal_gpio_ioctl(I2C_SDA_port, I2C_SDA_pin, gpio_ioctl_af_4);
     ExitOnError(err);
 
-    err = hal_gpio_ioctl(BSP_I2C2_SDA_PORT, BSP_I2C2_SDA_PIN, gpio_ioctl_alternate);
+    err = hal_gpio_ioctl(I2C_SDA_port, I2C_SDA_pin, gpio_ioctl_alternate);
     ExitOnError(err);
 
-    err = hal_gpio_ioctl(BSP_I2C2_SDA_PORT, BSP_I2C2_SDA_PIN, gpio_ioctl_open_drain);
+    err = hal_gpio_ioctl(I2C_SDA_port, I2C_SDA_pin, gpio_ioctl_open_drain);
     ExitOnError(err);
 
-    err = hal_gpio_ioctl(BSP_I2C2_SDA_PORT, BSP_I2C2_SDA_PIN, gpio_ioctl_very_high_speed);
-    ExitOnError(err);
-
-    err = hal_gpio_ioctl(BSP_I2C2_SDA_PORT, BSP_I2C2_SDA_PIN, gpio_ioctl_af_4);
+    err = hal_gpio_ioctl(I2C_SDA_port, I2C_SDA_pin, gpio_ioctl_very_high_speed);
     ExitOnError(err);
 
     /* SCL GPIO */
     // No internal pull-ups
-    err = hal_gpio_open(BSP_I2C2_SCL_PORT);
+    err = hal_gpio_open(I2C_SCL_port);
     ExitOnError(err);
 
-    err = hal_gpio_ioctl(BSP_I2C2_SCL_PORT, BSP_I2C2_SCL_PIN, gpio_ioctl_output);
+    err = hal_gpio_ioctl(I2C_SCL_port, I2C_SCL_pin, gpio_ioctl_af_4);
     ExitOnError(err);
 
-    err = hal_gpio_ioctl(BSP_I2C2_SCL_PORT, BSP_I2C2_SCL_PIN, gpio_ioctl_alternate);
+    err = hal_gpio_ioctl(I2C_SCL_port, I2C_SCL_pin, gpio_ioctl_alternate);
     ExitOnError(err);
 
-    err = hal_gpio_ioctl(BSP_I2C2_SCL_PORT, BSP_I2C2_SCL_PIN, gpio_ioctl_open_drain);
+    err = hal_gpio_ioctl(I2C_SCL_port, I2C_SCL_pin, gpio_ioctl_open_drain);
     ExitOnError(err);
 
-    err = hal_gpio_ioctl(BSP_I2C2_SCL_PORT, BSP_I2C2_SCL_PIN, gpio_ioctl_very_high_speed);
+    err = hal_gpio_ioctl(I2C_SCL_port, I2C_SCL_pin, gpio_ioctl_very_high_speed);
     ExitOnError(err);
 
-    err = hal_gpio_ioctl(BSP_I2C2_SCL_PORT, BSP_I2C2_SCL_PIN, gpio_ioctl_af_4);
-    ExitOnError(err);
-
-    err = hal_i2c_open(I2C2, I2C_mode_master);
+    /* I2C peripherial */
+    err = hal_i2c_open(I2C, I2C_mode_master, I2C_speed_100kHz);
     ExitOnError(err);
 
 exit:
@@ -55,8 +50,12 @@ exit:
 int bsp_init_periphs() {
     int err = 0;
 
+    /* Init I2C1 */
+    err = init_i2c(I2C1, BSP_I2C1_SDA_PORT, BSP_I2C1_SDA_PIN, BSP_I2C1_SCL_PORT, BSP_I2C1_SCL_PIN);
+    ExitOnError(err);
+
     /* Init I2C2 */
-    err = init_i2c2();
+    err = init_i2c(I2C2, BSP_I2C2_SDA_PORT, BSP_I2C2_SDA_PIN, BSP_I2C2_SCL_PORT, BSP_I2C2_SCL_PIN);
     ExitOnError(err);
 
 exit:
--- apps/stm32u5/u585xx_bring-up/main.c
+++ apps/stm32u5/u585xx_bring-up/main.c
@@ -5,9 +5,55 @@
 #include "delay.h"
 #include "gpio.h"
 #include "logger.h"
+#include "i2c.h"
+
+void error_blink() {
+    int blink_rate = 300;
+    bool led_status = true;
+    for (int i = 0; i < blink_rate; i += 50) {
+        led_status = !led_status;
+        hal_gpio_write(BSP_RED_LED_PORT, BSP_RED_LED_PIN, led_status);
+        delay_ms(blink_rate - i);
+    }
+}
+
+void hts221_simple_test(void) {
+    // This Function is only for I2C bring-up testing
+    // remove it in futre
+    uint8_t hts221_dev_addr         = 0xBE;
+    uint8_t hts221_who_am_i_reg     = 0x0F;
+    uint8_t hts221_ctrl_reg1_reg    = 0x20;
+
+    uint8_t hts221_rx = 0;
+    uint8_t hts221_tx = 0;
+
+
+    hal_i2c_master_read(I2C2, hts221_dev_addr, hts221_who_am_i_reg, &hts221_rx, 1);
+    if(hts221_rx != 0xBC) {
+        error_blink();
+        return;
+    }
+
+    hal_i2c_master_read(I2C2, hts221_dev_addr, hts221_ctrl_reg1_reg, &hts221_rx, 1);
+    // Can be on after warm reset
+    if((hts221_rx != 0x00) && (hts221_rx != 0x80)) {
+        error_blink();
+        return;
+    }
+
+    hts221_tx = hts221_rx | 0b10000000;  // Power on
+    hal_i2c_master_write(I2C2, hts221_dev_addr, hts221_ctrl_reg1_reg, &hts221_tx, 1);
+
+    hal_i2c_master_read(I2C2, hts221_dev_addr, hts221_ctrl_reg1_reg, &hts221_rx, 1);
+    if(hts221_rx != hts221_tx) {
+        error_blink();
+        return;
+    }
+}
 
 int main() {
     Assert(bsp_init(), 0);
+    hts221_simple_test();
 
     while (1) {
         bool r_value = !hal_gpio_read(BSP_USER_BUTTON_PORT, BSP_USER_BUTTON_PIN);
